0304eaf4e3cbba1453f62df2abcd67ec7d7f6223
brb7nu <brb7nu@virginia.edu>
2015-12-08 13:32:11 -0500

    Steven add pin logic. Still needs conditionals

--- main.c
+++ main.c
@@ -17,23 +17,161 @@
 #define ROW_8 BIT0
 #define ROW_9 BIT1
 #define ROW_10 BIT2
-// use P1.6 as the analog voltage in for music
-#define V_IN BIT6
+// use P1.7 as the analog voltage in for music
+#define V_IN BIT7
 
 
 // begin function definitions (we can move these to different file later)
 void initPins(void);
+void turnOnRows(void);
 
 int main(void) {
 
 	// Stop watchdog timer
 	WDTCTL = WDTPW | WDTHOLD;
-
+	DCOCTL = CALDCO_16MHZ;  			// |Set clock speed to 16 MHz|
+	BCSCTL1 = CALBC1_16MHZ;  			// |                        |
 	// begin main loop
 	while (1) {
-
+		turnOnRows();
 	}
 	return 0;
+}
+
+void turnOnRows() {
+
+	if (/*conditions for row 1 on only*/) {
+		P1OUT |= ROW_1;
+		P1OUT &= ~ROW_2;
+		P1OUT &= ~ROW_3;
+		P1OUT &= ~ROW_4;
+		P1OUT &= ~ROW_5;
+		P1OUT &= ~ROW_6;
+		P1OUT &= ~ROW_7;
+		P2OUT &= ~ROW_8;
+		P2OUT &= ~ROW_9;
+		P2OUT &= ~ROW_10;
+	}
+	else if (/*conditions for first 2 rows*/) {
+		P1OUT |= ROW_1;
+		P1OUT |= ROW_2;
+		P1OUT &= ~ROW_3;
+		P1OUT &= ~ROW_4;
+		P1OUT &= ~ROW_5;
+		P1OUT &= ~ROW_6;
+		P1OUT &= ~ROW_7;
+		P2OUT &= ~ROW_8;
+		P2OUT &= ~ROW_9;
+		P2OUT &= ~ROW_10;
+	}
+	else if (/*conditions for first 3 rows*/) {
+		P1OUT |= ROW_1;
+		P1OUT |= ROW_2;
+		P1OUT |= ROW_3;
+		P1OUT &= ~ROW_4;
+		P1OUT &= ~ROW_5;
+		P1OUT &= ~ROW_6;
+		P1OUT &= ~ROW_7;
+		P2OUT &= ~ROW_8;
+		P2OUT &= ~ROW_9;
+		P2OUT &= ~ROW_10;
+	}
+	else if (/*conditions for first 4 rows*/) {
+		P1OUT |= ROW_1;
+		P1OUT |= ROW_2;
+		P1OUT |= ROW_3;
+		P1OUT |= ROW_4;
+		P1OUT &= ~ROW_5;
+		P1OUT &= ~ROW_6;
+		P1OUT &= ~ROW_7;
+		P2OUT &= ~ROW_8;
+		P2OUT &= ~ROW_9;
+		P2OUT &= ~ROW_10;
+	}
+	else if (/*conditions for first 5 rows*/) {
+		P1OUT |= ROW_1;
+		P1OUT |= ROW_2;
+		P1OUT |= ROW_3;
+		P1OUT |= ROW_4;
+		P1OUT |= ROW_5;
+		P1OUT &= ~ROW_6;
+		P1OUT &= ~ROW_7;
+		P2OUT &= ~ROW_8;
+		P2OUT &= ~ROW_9;
+		P2OUT &= ~ROW_10;
+	}
+	else if (/*conditions for first 6 rows*/) {
+		P1OUT |= ROW_1;
+		P1OUT |= ROW_2;
+		P1OUT |= ROW_3;
+		P1OUT |= ROW_4;
+		P1OUT |= ROW_5;
+		P1OUT |= ROW_6;
+		P1OUT &= ~ROW_7;
+		P2OUT &= ~ROW_8;
+		P2OUT &= ~ROW_9;
+		P2OUT &= ~ROW_10;
+	}
+	else if (/*conditions for first 7 rows*/) {
+		P1OUT |= ROW_1;
+		P1OUT |= ROW_2;
+		P1OUT |= ROW_3;
+		P1OUT |= ROW_4;
+		P1OUT |= ROW_5;
+		P1OUT |= ROW_6;
+		P1OUT |= ROW_7;
+		P2OUT &= ~ROW_8;
+		P2OUT &= ~ROW_9;
+		P2OUT &= ~ROW_10;
+	}
+	else if (/*conditions for first 8 rows*/) {
+		P1OUT |= ROW_1;
+		P1OUT |= ROW_2;
+		P1OUT |= ROW_3;
+		P1OUT |= ROW_4;
+		P1OUT |= ROW_5;
+		P1OUT |= ROW_6;
+		P1OUT |= ROW_7;
+		P2OUT |= ROW_8;
+		P2OUT &= ~ROW_9;
+		P2OUT &= ~ROW_10;
+	}
+	else if (/*conditions for first 9 rows*/) {
+		P1OUT |= ROW_1;
+		P1OUT |= ROW_2;
+		P1OUT |= ROW_3;
+		P1OUT |= ROW_4;
+		P1OUT |= ROW_5;
+		P1OUT |= ROW_6;
+		P1OUT |= ROW_7;
+		P2OUT |= ROW_8;
+		P2OUT |= ROW_9;
+		P2OUT &= ~ROW_10;
+	}
+	else if (/*conditions for all 10 rows*/) {
+		P1OUT |= ROW_1;
+		P1OUT |= ROW_2;
+		P1OUT |= ROW_3;
+		P1OUT |= ROW_4;
+		P1OUT |= ROW_5;
+		P1OUT |= ROW_6;
+		P1OUT |= ROW_7;
+		P2OUT |= ROW_8;
+		P2OUT |= ROW_9;
+		P2OUT |= ROW_10;
+	}
+	else { // no/(very low) amplitude
+		P1OUT |= ROW_1;
+		P1OUT &= ~ROW_2;
+		P1OUT &= ~ROW_3;
+		P1OUT &= ~ROW_4;
+		P1OUT &= ~ROW_5;
+		P1OUT &= ~ROW_6;
+		P1OUT &= ~ROW_7;
+		P2OUT &= ~ROW_8;
+		P2OUT &= ~ROW_9;
+		P2OUT &= ~ROW_10;
+	}
 }
 
 void initPins(void) {
@@ -66,4 +204,6 @@ void initPins(void) {
 	P2DIR |= ROW_8;
 	P2DIR |= ROW_9;
 	P2DIR |= ROW_10;
+	// Enable Analog input for ADC on P1.7
+	ADC10AE0 |= BIT7;
 }
