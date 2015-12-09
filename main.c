42800eae1d5431a8afd42b732628f67f95e6dd72
brb7nu <brb7nu@virginia.edu>
2015-12-08 21:10:27 -0500

    change ports and pins for current setup

--- main.c
+++ main.c
@@ -4,22 +4,28 @@
  * Ben Browning, Steven Jenny, Will Cray
  */
 
-// define the different output pins corresponding to the different rows
-// on the V sabres
-#define ROW_1 BIT0
-#define ROW_2 BIT1
-#define ROW_3 BIT2
-#define ROW_4 BIT3
-#define ROW_5 BIT4
-#define ROW_6 BIT5
-#define ROW_7 BIT6
-// begin using P2 pins
-#define ROW_8 BIT0
-#define ROW_9 BIT1
-#define ROW_10 BIT2
-// use P1.7 as the analog voltage in for music
-#define V_IN BIT7
+// Define the different output pins corresponding to the different rows
+// on the V sabres.
 
+// port 1 pins
+#define RED_LED_P1 BIT0 // P1.0
+#define ROW_8_P1 BIT1 // P1.1
+#define ROW_7_P1 BIT2 // P1.2
+#define ROW_6_P1 BIT3 // P1.3
+#define ROW_1_P1 BIT4 // P1.4
+#define ROW_4_P1 BIT5 // P1.5
+#define ANALOG_IN_P1 BIT6 // P1.6 analog voltage in for music
+#define UNUSED_P1 BIT7 // P1.7
+
+// begin using P2 pins
+#define ROW_0_P2 BIT0 // P2.0
+#define ROW_3_P2 BIT1 // P2.1
+#define ROW_2_P2 BIT2 // P2.2
+#define ROW_9_P2 BIT3 // P2.3
+#define ROW_5_P2 BIT4 // P2.4
+#define UNUSED_P2 BIT5 // P2.5
+
+volatile int rowToLight;
 
 // begin function definitions (we can move these to different file later)
 void initPins(void);
@@ -31,6 +37,9 @@ int main(void) {
 	WDTCTL = WDTPW | WDTHOLD;
 	DCOCTL = CALDCO_16MHZ;  			// |Set clock speed to 16 MHz|
 	BCSCTL1 = CALBC1_16MHZ;  			// |                         |
+
+	rowToLight = 0;
+
 	// begin main loop
 	while (1) {
 		turnOnRows();
@@ -39,160 +48,171 @@ int main(void) {
 }
 
 void turnOnRows() {
-	if (/*conditions for row 1 on only*/) {
-		P1OUT |= ROW_1;
-		P1OUT &= ~ROW_2;
-		P1OUT &= ~ROW_3;
-		P1OUT &= ~ROW_4;
-		P1OUT &= ~ROW_5;
-		P1OUT &= ~ROW_6;
-		P1OUT &= ~ROW_7;
-		P2OUT &= ~ROW_8;
-		P2OUT &= ~ROW_9;
-		P2OUT &= ~ROW_10;
+	if (/*conditions for no rows*/) {
+		P2OUT &= ~ROW_0_P2;
+		P1OUT &= ~ROW_1_P1;
+		P2OUT &= ~ROW_2_P2;
+		P2OUT &= ~ROW_3_P2;
+		P1OUT &= ~ROW_4_P1;
+		P2OUT &= ~ROW_5_P2;
+		P1OUT &= ~ROW_6_P1;
+		P1OUT &= ~ROW_7_P1;
+		P1OUT &= ~ROW_8_P1;
+		P2OUT &= ~ROW_9_P2;
+	} else if (/*conditions for first row only*/) {
+		P2OUT |= ROW_0_P2;
+		P1OUT &= ~ROW_1_P1;
+		P2OUT &= ~ROW_2_P2;
+		P2OUT &= ~ROW_3_P2;
+		P1OUT &= ~ROW_4_P1;
+		P2OUT &= ~ROW_5_P2;
+		P1OUT &= ~ROW_6_P1;
+		P1OUT &= ~ROW_7_P1;
+		P1OUT &= ~ROW_8_P1;
+		P2OUT &= ~ROW_9_P2;
 	} else if (/*conditions for first 2 rows*/) {
-		P1OUT |= ROW_1;
-		P1OUT |= ROW_2;
-		P1OUT &= ~ROW_3;
-		P1OUT &= ~ROW_4;
-		P1OUT &= ~ROW_5;
-		P1OUT &= ~ROW_6;
-		P1OUT &= ~ROW_7;
-		P2OUT &= ~ROW_8;
-		P2OUT &= ~ROW_9;
-		P2OUT &= ~ROW_10;
+		P2OUT |= ROW_0_P2;
+		P1OUT |= ROW_1_P1;
+		P2OUT &= ~ROW_2_P2;
+		P2OUT &= ~ROW_3_P2;
+		P1OUT &= ~ROW_4_P1;
+		P2OUT &= ~ROW_5_P2;
+		P1OUT &= ~ROW_6_P1;
+		P1OUT &= ~ROW_7_P1;
+		P1OUT &= ~ROW_8_P1;
+		P2OUT &= ~ROW_9_P2;
 	} else if (/*conditions for first 3 rows*/) {
-		P1OUT |= ROW_1;
-		P1OUT |= ROW_2;
-		P1OUT |= ROW_3;
-		P1OUT &= ~ROW_4;
-		P1OUT &= ~ROW_5;
-		P1OUT &= ~ROW_6;
-		P1OUT &= ~ROW_7;
-		P2OUT &= ~ROW_8;
-		P2OUT &= ~ROW_9;
-		P2OUT &= ~ROW_10;
+		P2OUT |= ROW_0_P2;
+		P1OUT |= ROW_1_P1;
+		P2OUT |= ROW_2_P2;
+		P2OUT &= ~ROW_3_P2;
+		P1OUT &= ~ROW_4_P1;
+		P2OUT &= ~ROW_5_P2;
+		P1OUT &= ~ROW_6_P1;
+		P1OUT &= ~ROW_7_P1;
+		P1OUT &= ~ROW_8_P1;
+		P2OUT &= ~ROW_9_P2;
 	} else if (/*conditions for first 4 rows*/) {
-		P1OUT |= ROW_1;
-		P1OUT |= ROW_2;
-		P1OUT |= ROW_3;
-		P1OUT |= ROW_4;
-		P1OUT &= ~ROW_5;
-		P1OUT &= ~ROW_6;
-		P1OUT &= ~ROW_7;
-		P2OUT &= ~ROW_8;
-		P2OUT &= ~ROW_9;
-		P2OUT &= ~ROW_10;
+		P2OUT |= ROW_0_P2;
+		P1OUT |= ROW_1_P1;
+		P2OUT |= ROW_2_P2;
+		P2OUT |= ROW_3_P2;
+		P1OUT &= ~ROW_4_P1;
+		P2OUT &= ~ROW_5_P2;
+		P1OUT &= ~ROW_6_P1;
+		P1OUT &= ~ROW_7_P1;
+		P1OUT &= ~ROW_8_P1;
+		P2OUT &= ~ROW_9_P2;
 	} else if (/*conditions for first 5 rows*/) {
-		P1OUT |= ROW_1;
-		P1OUT |= ROW_2;
-		P1OUT |= ROW_3;
-		P1OUT |= ROW_4;
-		P1OUT |= ROW_5;
-		P1OUT &= ~ROW_6;
-		P1OUT &= ~ROW_7;
-		P2OUT &= ~ROW_8;
-		P2OUT &= ~ROW_9;
-		P2OUT &= ~ROW_10;
+		P2OUT |= ROW_0_P2;
+		P1OUT |= ROW_1_P1;
+		P2OUT |= ROW_2_P2;
+		P2OUT |= ROW_3_P2;
+		P1OUT |= ROW_4_P1;
+		P2OUT &= ~ROW_5_P2;
+		P1OUT &= ~ROW_6_P1;
+		P1OUT &= ~ROW_7_P1;
+		P1OUT &= ~ROW_8_P1;
+		P2OUT &= ~ROW_9_P2;
 	} else if (/*conditions for first 6 rows*/) {
-		P1OUT |= ROW_1;
-		P1OUT |= ROW_2;
-		P1OUT |= ROW_3;
-		P1OUT |= ROW_4;
-		P1OUT |= ROW_5;
-		P1OUT |= ROW_6;
-		P1OUT &= ~ROW_7;
-		P2OUT &= ~ROW_8;
-		P2OUT &= ~ROW_9;
-		P2OUT &= ~ROW_10;
+		P2OUT |= ROW_0_P2;
+		P1OUT |= ROW_1_P1;
+		P2OUT |= ROW_2_P2;
+		P2OUT |= ROW_3_P2;
+		P1OUT |= ROW_4_P1;
+		P2OUT |= ROW_5_P2;
+		P1OUT &= ~ROW_6_P1;
+		P1OUT &= ~ROW_7_P1;
+		P1OUT &= ~ROW_8_P1;
+		P2OUT &= ~ROW_9_P2;
 	} else if (/*conditions for first 7 rows*/) {
-		P1OUT |= ROW_1;
-		P1OUT |= ROW_2;
-		P1OUT |= ROW_3;
-		P1OUT |= ROW_4;
-		P1OUT |= ROW_5;
-		P1OUT |= ROW_6;
-		P1OUT |= ROW_7;
-		P2OUT &= ~ROW_8;
-		P2OUT &= ~ROW_9;
-		P2OUT &= ~ROW_10;
+		P2OUT |= ROW_0_P2;
+		P1OUT |= ROW_1_P1;
+		P2OUT |= ROW_2_P2;
+		P2OUT |= ROW_3_P2;
+		P1OUT |= ROW_4_P1;
+		P2OUT |= ROW_5_P2;
+		P1OUT |= ROW_6_P1;
+		P1OUT &= ~ROW_7_P1;
+		P1OUT &= ~ROW_8_P1;
+		P2OUT &= ~ROW_9_P2;
 	} else if (/*conditions for first 8 rows*/) {
-		P1OUT |= ROW_1;
-		P1OUT |= ROW_2;
-		P1OUT |= ROW_3;
-		P1OUT |= ROW_4;
-		P1OUT |= ROW_5;
-		P1OUT |= ROW_6;
-		P1OUT |= ROW_7;
-		P2OUT |= ROW_8;
-		P2OUT &= ~ROW_9;
-		P2OUT &= ~ROW_10;
+		P2OUT |= ROW_0_P2;
+		P1OUT |= ROW_1_P1;
+		P2OUT |= ROW_2_P2;
+		P2OUT |= ROW_3_P2;
+		P1OUT |= ROW_4_P1;
+		P2OUT |= ROW_5_P2;
+		P1OUT |= ROW_6_P1;
+		P1OUT |= ROW_7_P1;
+		P1OUT &= ~ROW_8_P1;
+		P2OUT &= ~ROW_9_P2;
 	} else if (/*conditions for first 9 rows*/) {
-		P1OUT |= ROW_1;
-		P1OUT |= ROW_2;
-		P1OUT |= ROW_3;
-		P1OUT |= ROW_4;
-		P1OUT |= ROW_5;
-		P1OUT |= ROW_6;
-		P1OUT |= ROW_7;
-		P2OUT |= ROW_8;
-		P2OUT |= ROW_9;
-		P2OUT &= ~ROW_10;
+		P2OUT |= ROW_0_P2;
+		P1OUT |= ROW_1_P1;
+		P2OUT |= ROW_2_P2;
+		P2OUT |= ROW_3_P2;
+		P1OUT |= ROW_4_P1;
+		P2OUT |= ROW_5_P2;
+		P1OUT |= ROW_6_P1;
+		P1OUT |= ROW_7_P1;
+		P1OUT |= ROW_8_P1;
+		P2OUT &= ~ROW_9_P2;
 	} else if (/*conditions for all 10 rows*/) {
-		P1OUT |= ROW_1;
-		P1OUT |= ROW_2;
-		P1OUT |= ROW_3;
-		P1OUT |= ROW_4;
-		P1OUT |= ROW_5;
-		P1OUT |= ROW_6;
-		P1OUT |= ROW_7;
-		P2OUT |= ROW_8;
-		P2OUT |= ROW_9;
-		P2OUT |= ROW_10;
-	} else { // no/(very low) amplitude
-		P1OUT |= ROW_1;
-		P1OUT &= ~ROW_2;
-		P1OUT &= ~ROW_3;
-		P1OUT &= ~ROW_4;
-		P1OUT &= ~ROW_5;
-		P1OUT &= ~ROW_6;
-		P1OUT &= ~ROW_7;
-		P2OUT &= ~ROW_8;
-		P2OUT &= ~ROW_9;
-		P2OUT &= ~ROW_10;
+		P2OUT |= ROW_0_P2;
+		P1OUT |= ROW_1_P1;
+		P2OUT |= ROW_2_P2;
+		P2OUT |= ROW_3_P2;
+		P1OUT |= ROW_4_P1;
+		P2OUT |= ROW_5_P2;
+		P1OUT |= ROW_6_P1;
+		P1OUT |= ROW_7_P1;
+		P1OUT |= ROW_8_P1;
+		P2OUT |= ROW_9_P2;
+	} else {
+		// some error
+		P2OUT &= ~ROW_0_P2;
+		P1OUT |= ROW_1_P1;
+		P2OUT &= ~ROW_2_P2;
+		P2OUT |= ROW_3_P2;
+		P1OUT &= ~ROW_4_P1;
+		P2OUT |= ROW_5_P2;
+		P1OUT &= ~ROW_6_P1;
+		P1OUT |= ROW_7_P1;
+		P1OUT &= ~ROW_8_P1;
+		P2OUT |= ROW_9_P2;
 	}
 }
 
 void initPins(void) {
 
 	// set P1.6 as analog input for the music
-	P1DIR &= ~V_IN;
+	P1DIR &= ~ANALOG_IN_P1;
 
 	// set all LED rows to low at the beginning
-	P1OUT &= ~ROW_1;
-	P1OUT &= ~ROW_2;
-	P1OUT &= ~ROW_3;
-	P1OUT &= ~ROW_4;
-	P1OUT &= ~ROW_5;
-	P1OUT &= ~ROW_6;
-	P1OUT &= ~ROW_7;
-	// begin using P2 pins
-	P2OUT &= ~ROW_8;
-	P2OUT &= ~ROW_9;
-	P2OUT &= ~ROW_10;
+	P2OUT &= ~ROW_0_P2;
+	P1OUT &= ~ROW_1_P1;
+	P2OUT &= ~ROW_2_P2;
+	P2OUT &= ~ROW_3_P2;
+	P1OUT &= ~ROW_4_P1;
+	P2OUT &= ~ROW_5_P2;
+	P1OUT &= ~ROW_6_P1;
+	P1OUT &= ~ROW_7_P1;
+	P1OUT &= ~ROW_8_P1;
+	P2OUT &= ~ROW_9_P2;
+
 	// set all pins to output direction
-	P1DIR |= ROW_0;
-	P1DIR |= ROW_1;
-	P1DIR |= ROW_2;
-	P1DIR |= ROW_3;
-	P1DIR |= ROW_4;
-	P1DIR |= ROW_5;
-	// begin using P2 pins
-	P2DIR |= ROW_6;
-	P2DIR |= ROW_7;
-	P2DIR |= ROW_8;
-	P2DIR |= ROW_9;
-	P2DIR |= ROW_10;
-	// Enable Analog input for ADC on P1.7
-	ADC10AE0 |= BIT7;
+	P2DIR |= ROW_0_P2;
+	P1DIR |= ROW_1_P1;
+	P2DIR |= ROW_2_P2;
+	P2DIR |= ROW_3_P2;
+	P1DIR |= ROW_4_P1;
+	P2DIR |= ROW_5_P2;
+	P1DIR |= ROW_6_P1;
+	P1DIR |= ROW_7_P1;
+	P1DIR |= ROW_8_P1;
+	P2DIR |= ROW_9_P2;
+
+	// Enable Analog input for ADC on P1.6
+	ADC10AE0 |= ANALOG_IN_P1;
 }
