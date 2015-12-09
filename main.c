e463595810c19150b7caaacbeba55e3ee0225c01
brb7nu <brb7nu@virginia.edu>
2015-12-08 20:37:48 -0500

    formatting fixes

--- main.c
+++ main.c
@@ -30,7 +30,7 @@ int main(void) {
 	// Stop watchdog timer
 	WDTCTL = WDTPW | WDTHOLD;
 	DCOCTL = CALDCO_16MHZ;  			// |Set clock speed to 16 MHz|
-	BCSCTL1 = CALBC1_16MHZ;  			// |                        |
+	BCSCTL1 = CALBC1_16MHZ;  			// |                         |
 	// begin main loop
 	while (1) {
 		turnOnRows();
@@ -39,7 +39,6 @@ int main(void) {
 }
 
 void turnOnRows() {
-
 	if (/*conditions for row 1 on only*/) {
 		P1OUT |= ROW_1;
 		P1OUT &= ~ROW_2;
@@ -51,8 +50,7 @@ void turnOnRows() {
 		P2OUT &= ~ROW_8;
 		P2OUT &= ~ROW_9;
 		P2OUT &= ~ROW_10;
-	}
-	else if (/*conditions for first 2 rows*/) {
+	} else if (/*conditions for first 2 rows*/) {
 		P1OUT |= ROW_1;
 		P1OUT |= ROW_2;
 		P1OUT &= ~ROW_3;
@@ -63,8 +61,7 @@ void turnOnRows() {
 		P2OUT &= ~ROW_8;
 		P2OUT &= ~ROW_9;
 		P2OUT &= ~ROW_10;
-	}
-	else if (/*conditions for first 3 rows*/) {
+	} else if (/*conditions for first 3 rows*/) {
 		P1OUT |= ROW_1;
 		P1OUT |= ROW_2;
 		P1OUT |= ROW_3;
@@ -75,8 +72,7 @@ void turnOnRows() {
 		P2OUT &= ~ROW_8;
 		P2OUT &= ~ROW_9;
 		P2OUT &= ~ROW_10;
-	}
-	else if (/*conditions for first 4 rows*/) {
+	} else if (/*conditions for first 4 rows*/) {
 		P1OUT |= ROW_1;
 		P1OUT |= ROW_2;
 		P1OUT |= ROW_3;
@@ -87,8 +83,7 @@ void turnOnRows() {
 		P2OUT &= ~ROW_8;
 		P2OUT &= ~ROW_9;
 		P2OUT &= ~ROW_10;
-	}
-	else if (/*conditions for first 5 rows*/) {
+	} else if (/*conditions for first 5 rows*/) {
 		P1OUT |= ROW_1;
 		P1OUT |= ROW_2;
 		P1OUT |= ROW_3;
@@ -99,8 +94,7 @@ void turnOnRows() {
 		P2OUT &= ~ROW_8;
 		P2OUT &= ~ROW_9;
 		P2OUT &= ~ROW_10;
-	}
-	else if (/*conditions for first 6 rows*/) {
+	} else if (/*conditions for first 6 rows*/) {
 		P1OUT |= ROW_1;
 		P1OUT |= ROW_2;
 		P1OUT |= ROW_3;
@@ -111,8 +105,7 @@ void turnOnRows() {
 		P2OUT &= ~ROW_8;
 		P2OUT &= ~ROW_9;
 		P2OUT &= ~ROW_10;
-	}
-	else if (/*conditions for first 7 rows*/) {
+	} else if (/*conditions for first 7 rows*/) {
 		P1OUT |= ROW_1;
 		P1OUT |= ROW_2;
 		P1OUT |= ROW_3;
@@ -123,8 +116,7 @@ void turnOnRows() {
 		P2OUT &= ~ROW_8;
 		P2OUT &= ~ROW_9;
 		P2OUT &= ~ROW_10;
-	}
-	else if (/*conditions for first 8 rows*/) {
+	} else if (/*conditions for first 8 rows*/) {
 		P1OUT |= ROW_1;
 		P1OUT |= ROW_2;
 		P1OUT |= ROW_3;
@@ -135,8 +127,7 @@ void turnOnRows() {
 		P2OUT |= ROW_8;
 		P2OUT &= ~ROW_9;
 		P2OUT &= ~ROW_10;
-	}
-	else if (/*conditions for first 9 rows*/) {
+	} else if (/*conditions for first 9 rows*/) {
 		P1OUT |= ROW_1;
 		P1OUT |= ROW_2;
 		P1OUT |= ROW_3;
@@ -147,8 +138,7 @@ void turnOnRows() {
 		P2OUT |= ROW_8;
 		P2OUT |= ROW_9;
 		P2OUT &= ~ROW_10;
-	}
-	else if (/*conditions for all 10 rows*/) {
+	} else if (/*conditions for all 10 rows*/) {
 		P1OUT |= ROW_1;
 		P1OUT |= ROW_2;
 		P1OUT |= ROW_3;
@@ -159,8 +149,7 @@ void turnOnRows() {
 		P2OUT |= ROW_8;
 		P2OUT |= ROW_9;
 		P2OUT |= ROW_10;
-	}
-	else { // no/(very low) amplitude
+	} else { // no/(very low) amplitude
 		P1OUT |= ROW_1;
 		P1OUT &= ~ROW_2;
 		P1OUT &= ~ROW_3;
