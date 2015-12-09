63d2ebb554ac44b81ef7dcb6790fd635da0e42fb
wcc8fs <wcc8fs@virginia.edu>
2015-12-09 13:55:02 -0500

    working everything

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include <msp430.h> 
+#include <msp430.h>
 /*
  * ECE 3430 - FINAL PROJECT - LIGHT SABRES
  * Ben Browning, Steven Jenny, Will Cray
@@ -6,7 +6,6 @@
 
 // Define the different output pins corresponding to the different rows
 // on the V sabres.
-
 // port 1 pins
 #define RED_LED_P1 BIT0 // P1.0
 #define ROW_8_P1 BIT1 // P1.1
@@ -25,11 +24,22 @@
 #define ROW_5_P2 BIT4 // P2.4
 #define UNUSED_P2 BIT5 // P2.5
 
+#define NUM_SAMPLES 8 // if you change the number of samples, you need to change the number of bit shifts you do to take the average on line 61
+// change the value to log2(NUM_SAMPLES)
+
 volatile int rowsToLight;
+int n_x = 0; // index value
+int j; // for loop variable
+long v_in = 0; // value being read in from headphone jack
+int sum = 0; // sum for the average calculation
+long average =0; // the average value of the array
+int samples[NUM_SAMPLES]; // instantiate array
 
 // begin function definitions (we can move these to different file later)
 void initPins(void);
 void turnOnRows(void);
+void ConfigureAdc(void);		// header for function
+void filter(void);				// header for function
 
 int main(void) {
 
@@ -55,6 +65,7 @@ int main(void) {
 						// when their corresponding CCIE bit and
 						// the GIE bit are set." - UserGuide
 
+	/*
 	// Set ADC to measure input channel 7, aka pin 1.7
 	// Also set ADC to measure at 1/4 the normal rate
 	ADC10CTL1 = INCH_7 + ADC10DIV_3;
@@ -64,20 +75,21 @@ int main(void) {
 	// Also set Reference generator to ON
 	// Also set ADC10 to ON
 	ADC10CTL0 = SREF_1 + ADC10SHT_3 + REF2_5V + REFON + ADC10ON;
+	*/
+
+	ConfigureAdc();					// call the configure function
 
 	__delay_cycles(1000); // wait for ADC Ref to settle
 
+/*
 	// Set ADC to enable conversion
 	// Also set ADC to start converting
 	ADC10CTL0 |= ENC + ADC10SC;
-	calibrateValue = ADC10MEM;
 
-	// calibrate indices of samples
-	int i;
-	for (i = 0; i < NUM_SAMPLES; i++) {
-		samples[i] = calibrateValue;
-	}
-	average = calibrateValue;
+	// set P1.6 as analog in
+	P1SEL != BIT6;
+	v_in = ADC10MEM;
+  */
 
 	// right before entering main loop, Global Interrupt Enable
 	_BIS_SR(GIE);
@@ -88,13 +100,14 @@ int main(void) {
 
 	// begin main loop
 	while (1) {
+		rowsToLight = average; // LEDs lit 3 rows when no music playing but headphones plugged in.
 		turnOnRows();
 	}
 	return 0;
 }
 
 void turnOnRows() {
-	if (rowsToLight == 0) {
+	if (rowsToLight >= 0 && rowsToLight < 3) {
 		P2OUT &= ~ROW_0_P2;
 		P1OUT &= ~ROW_1_P1;
 		P2OUT &= ~ROW_2_P2;
@@ -105,7 +118,7 @@ void turnOnRows() {
 		P1OUT &= ~ROW_7_P1;
 		P1OUT &= ~ROW_8_P1;
 		P2OUT &= ~ROW_9_P2;
-	} else if (rowsToLight == 1) {
+	} else if (rowsToLight == 3) {
 		P2OUT |= ROW_0_P2;
 		P1OUT &= ~ROW_1_P1;
 		P2OUT &= ~ROW_2_P2;
@@ -116,7 +129,7 @@ void turnOnRows() {
 		P1OUT &= ~ROW_7_P1;
 		P1OUT &= ~ROW_8_P1;
 		P2OUT &= ~ROW_9_P2;
-	} else if (rowsToLight == 2) {
+	} else if (rowsToLight == 4) {
 		P2OUT |= ROW_0_P2;
 		P1OUT |= ROW_1_P1;
 		P2OUT &= ~ROW_2_P2;
@@ -127,7 +140,7 @@ void turnOnRows() {
 		P1OUT &= ~ROW_7_P1;
 		P1OUT &= ~ROW_8_P1;
 		P2OUT &= ~ROW_9_P2;
-	} else if (rowsToLight == 3) {
+	} else if (rowsToLight == 5) {
 		P2OUT |= ROW_0_P2;
 		P1OUT |= ROW_1_P1;
 		P2OUT |= ROW_2_P2;
@@ -138,7 +151,7 @@ void turnOnRows() {
 		P1OUT &= ~ROW_7_P1;
 		P1OUT &= ~ROW_8_P1;
 		P2OUT &= ~ROW_9_P2;
-	} else if (rowsToLight == 4) {
+	} else if (rowsToLight == 6) {
 		P2OUT |= ROW_0_P2;
 		P1OUT |= ROW_1_P1;
 		P2OUT |= ROW_2_P2;
@@ -149,7 +162,7 @@ void turnOnRows() {
 		P1OUT &= ~ROW_7_P1;
 		P1OUT &= ~ROW_8_P1;
 		P2OUT &= ~ROW_9_P2;
-	} else if (rowsToLight == 5) {
+	} else if (rowsToLight == 7) {
 		P2OUT |= ROW_0_P2;
 		P1OUT |= ROW_1_P1;
 		P2OUT |= ROW_2_P2;
@@ -160,7 +173,7 @@ void turnOnRows() {
 		P1OUT &= ~ROW_7_P1;
 		P1OUT &= ~ROW_8_P1;
 		P2OUT &= ~ROW_9_P2;
-	} else if (rowsToLight == 6) {
+	} else if (rowsToLight == 8) {
 		P2OUT |= ROW_0_P2;
 		P1OUT |= ROW_1_P1;
 		P2OUT |= ROW_2_P2;
@@ -171,7 +184,7 @@ void turnOnRows() {
 		P1OUT &= ~ROW_7_P1;
 		P1OUT &= ~ROW_8_P1;
 		P2OUT &= ~ROW_9_P2;
-	} else if (rowsToLight == 7) {
+	} else if (rowsToLight == 9) {
 		P2OUT |= ROW_0_P2;
 		P1OUT |= ROW_1_P1;
 		P2OUT |= ROW_2_P2;
@@ -182,7 +195,7 @@ void turnOnRows() {
 		P1OUT &= ~ROW_7_P1;
 		P1OUT &= ~ROW_8_P1;
 		P2OUT &= ~ROW_9_P2;
-	} else if (rowsToLight == 8) {
+	} else if (rowsToLight == 10) {
 		P2OUT |= ROW_0_P2;
 		P1OUT |= ROW_1_P1;
 		P2OUT |= ROW_2_P2;
@@ -193,7 +206,7 @@ void turnOnRows() {
 		P1OUT |= ROW_7_P1;
 		P1OUT &= ~ROW_8_P1;
 		P2OUT &= ~ROW_9_P2;
-	} else if (rowsToLight == 9) {
+	} else if (rowsToLight == 11) {
 		P2OUT |= ROW_0_P2;
 		P1OUT |= ROW_1_P1;
 		P2OUT |= ROW_2_P2;
@@ -204,7 +217,7 @@ void turnOnRows() {
 		P1OUT |= ROW_7_P1;
 		P1OUT |= ROW_8_P1;
 		P2OUT &= ~ROW_9_P2;
-	} else if (rowsToLight == 10) {
+	} else if (rowsToLight == 12) {
 		P2OUT |= ROW_0_P2;
 		P1OUT |= ROW_1_P1;
 		P2OUT |= ROW_2_P2;
@@ -258,39 +271,27 @@ void initPins(void) {
 	P1DIR |= ROW_7_P1;
 	P1DIR |= ROW_8_P1;
 	P2DIR |= ROW_9_P2;
-
-	// Enable Analog input for ADC on P1.6
-	ADC10AE0 |= ANALOG_IN_P1;
 }
 
-// Timer A interrupt service routine
-#pragma vector = TIMER0_A0_VECTOR
+#pragma vector = TIMER0_A0_VECTOR         // Timer A interrupt service routine, add code here for IN LAB
 __interrupt void TimerA0_routine(void) {
+	ADC10CTL0 |= ENC + ADC10SC; // sampling and conversion start to store data in memory
+	v_in = ADC10MEM; // extract data from the memory storage of the input voltage
+	sum -= samples[n_x];    // get rid of current value
+	samples[n_x] = v_in; // input v_in data here
+	sum += samples[n_x];    // add in new value to sum
+	n_x = (n_x + 1) % NUM_SAMPLES; // iterate through the array
+	filter();         // call filter function
+}
 
-	// this is run every 1/480Hz when Timer A hits comparison value
+void filter() {
+	_nop();             // do a no op
+	average = sum >> 3; // change value being bit shifted by to log2(NUM_SAMPLES)
+	// filter function optimized with bit shift function
+}
 
-	// red LED on
-	P1OUT |= BIT0;
-
-	// sampling and conversion start
-	ADC10CTL0 |= ENC + ADC10SC;
-
-	// keep track of the value we're about to overwrite
-	oldestReading = samples[index];
-
-	// sample temperature sensor
-	// store this sample in the oldest slot in the circular array 'samples'
-	samples[index] = ADC10MEM;
-
-	// calculate average of the samples in the sampling window
-	filter();
-
-	// update index "pointer" if it tries to roll off the end
-	// of the circular array
-	index = index + 1;
-	if (index == NUM_SAMPLES)
-		index = 0; // this is more efficient than modulus
-
-	// red LED off
-	P1OUT &= ~BIT0;
+void ConfigureAdc(void){
+	ADC10CTL1 = ADC10DIV_3 + INCH_6;		// divide clock by one
+	ADC10CTL0 = SREF_0 + ADC10SHT_3 + ADC10ON + REFON; //REF2_5V; // Vcc & Vss as reference, 64 clock cycles, turn on the ADC10 core, turn on the reference generator, make reference voltage 2.5 V
+	ADC10AE0 |= ANALOG_IN_P1;						// ADC input enable P1.6
 }
